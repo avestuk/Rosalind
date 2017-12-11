d8de9458c28f5f16ae67060dbbca652f3dc28ed1
Alex <avestuk@gmail.com>
2017-12-11 20:27:30 +0000

    prot.py complete - Could be cleaned up to give a better hint about which file is required

--- perm.py
+++ perm.py
@@ -2,3 +2,4 @@
 # Could use this to also figure out how many place holders the list needs
 # Begin creating lists by checking if an integer has already been in a position. I think that each int can be in a position n - 1 times
 # Create a cache to check against then check each position
+# itertools.product would probably make thi sall very easy!
--- prot.py
+++ prot.py
@@ -1,11 +1,12 @@
 from datasetImporter import create_filepath
 
 
+
 def rna_codons():
     rna_codon = {}
     with open(create_filepath()) as file:
         for line in file:
-            rna_codon[line.split()[0]] = line.split()[1]  # set
+            rna_codon[line.split()[0]] = line.split()[1]
         return rna_codon
 
 
@@ -18,8 +19,16 @@ def rna_string():
 def rna_codon_mapping():
     protein_string = ''
     rna_codon_map = rna_codons()
-    for x in rna_string()[::3]:
-        protein_string += rna_codon_map[x]
-    return protein_string
+    RNA_string = rna_string()
+    i = 0
+    while i < len(RNA_string):
+        if RNA_string[i:i + 3] in rna_codon_map.keys():
+            if rna_codon_map[RNA_string[i:i + 3]] == 'Stop':
+                break
+            slice = RNA_string[i:i + 3]
+            protein_string += rna_codon_map[RNA_string[i:i + 3]]
+            i += 3
+    print(protein_string)
+
 
 rna_codon_mapping()
